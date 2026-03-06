e810f4d01745376d08afabe566f1eb5227135a75
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-03-05 23:49:30 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install wisp-server-python --break-system-packages
+pip install wisp-python --break-system-packages
 python3 -m wisp.server --host 0.0.0.0 --port 10000 --static public --proxy socks5://tlaqdnkf-rotate:7gehk0l6v9li@p.webshare.io:80
