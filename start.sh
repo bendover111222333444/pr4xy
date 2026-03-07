26534795651c7a5b919e606d52ae135d0263db40
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-03-06 20:56:57 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-pip install wisp-python --break-system-packages
-./wireproxy -c wgcf-profile.conf &
-sleep 8
-python3 -m wisp.server --host 127.0.0.1 --port 5001 --proxy socks5://127.0.0.1:40000 &
+python3 -m wisp.server --host 127.0.0.1 --port 5001 &
 sleep 2
 node src/index.js
