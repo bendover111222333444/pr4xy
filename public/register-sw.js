8a03125a2a36e38c4a643338702445d2d0295d29
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-03-07 23:48:41 -0800

    Update register-sw.js

--- public/register-sw.js
+++ public/register-sw.js
@@ -14,25 +14,6 @@ async function registerSW() {
 		throw new Error("Your browser doesn't support service workers.");
 	}
 
-	const registration = await navigator.serviceWorker.register(stockSW);
-
-	// Wait for the service worker to be active and controlling the page
-	await new Promise((resolve) => {
-		if (registration.active) return resolve();
-		const sw = registration.installing || registration.waiting;
-		if (sw) {
-			sw.addEventListener("statechange", () => {
-				if (sw.state === "activated") resolve();
-			});
-		} else {
-			resolve();
-		}
-	});
-
-	// Ensure the SW is controlling this page
-	if (!navigator.serviceWorker.controller) {
-		await new Promise((resolve) => {
-			navigator.serviceWorker.addEventListener("controllerchange", resolve, { once: true });
-		});
-	}
+	await navigator.serviceWorker.register(stockSW);
+	await navigator.serviceWorker.ready;
 }
