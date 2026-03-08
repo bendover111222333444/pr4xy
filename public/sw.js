423643a31d4954512b55809a75a481e94058a1a1
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-03-08 01:32:17 -0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,30 @@
 importScripts("/scram-custom/scramjet.all.js");
+
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener("message", (event) => {
+    if (event.data === "abort") {
+        self.skipWaiting();
+    }
+});
+
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
+
 async function handleRequest(event) {
-	await scramjet.loadConfig();
-	if (scramjet.route(event)) {
-		return scramjet.fetch(event);
-	}
-	return fetch(event.request);
+    await scramjet.loadConfig();
+    if (scramjet.route(event)) {
+        return scramjet.fetch(event);
+    }
+    return fetch(event.request);
 }
+
 self.addEventListener("fetch", (event) => {
-	event.respondWith(handleRequest(event));
+    event.respondWith(handleRequest(event));
 });
